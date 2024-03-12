8928ba6d5922072c3fcd3d3824824613c56170c4
MTPGI <115469767+MTPGI@users.noreply.github.com>
2024-03-12 01:57:52 +0000

    Add files via upload

--- js/mtpgi38.js
+++ js/mtpgi38.js
@@ -3,17 +3,13 @@
 // HELPER FUNCTIONS
 
 const 
-	$id = id => {
-		return document.getElementById(id)
-	},
+	$id = id => document.getElementById(id),
 	$q = Element.prototype.$q = function(q) {
-		let r = (this instanceof Element ? this : document).querySelectorAll(q);
+		const r = (this instanceof Element ? this : document).querySelectorAll(q);
 		return /*r.length === 1 ? r[0] :*/ r.length ? Array.from(r) : null;
 	},
-	show = (el) => el.classList.remove('hide'),
-	hide = (el) => el.classList.add('hide'),
-	/*show = el => el.classList.remove('hide'),
-    hide = el => el.classList.add('hide'),*/
+	show = el => el.classList.remove("hide"),
+    hide = el => el.classList.add("hide"),
 	debounce = (fn, time) => {
 		time = time || 100;
 		let timer;
@@ -21,7 +17,7 @@ const
 			if (timer) clearTimeout(timer);
 			timer = setTimeout(fn, time, event);
 		}
-	},
+	}/*,
 	encodeLinks = () => {
 		function encodeAllCharacters(str) {
 			return Array.from(str).map(char => "%" + char.charCodeAt(0).toString(16).toUpperCase()).join("");
@@ -43,7 +39,7 @@ const
 			}
 		});
 		return document.documentElement.innerHTML;
-	};
+	}*/;
 
 // GLOBAL VARIABLES
 
@@ -92,29 +88,31 @@ const villes = {
     "VILLEFONTAINE":"553",
     "VIZILLE":"562",
     "VOIRON":"563"
-},
+}/*,
 classNames = {
-	email:'E-mail',
-	tel:'Téléphone',
-	url:'Site internet',
-	adr:'Adresse',
-	bus:'Transports en commun',
-	info:'Informations',
-	hours:'Horaires',
-	acc: 'Accessibilité',
-	facebook:'Compte Facebook',
-	twitter:' Compte Twitter',
-	instagram: 'Compte Instagram',
-	youtube: 'Chaîne Youtube'
-};
+	email: "E-mail",
+	tel: "Téléphone",
+	url: "Site internet",
+	adr: "Adresse",
+	bus: "Transports en commun",
+	info: "Informations",
+	hours: "Horaires",
+	acc: "Accessibilité",
+	facebook: "Compte Facebook",
+	twitter: "Compte Twitter",
+	instagram: "Compte Instagram",
+	youtube: "Chaîne Youtube",
+	linkedin: "Profil LinkedIn"
+}*/;
 
 function filtrerArticles() {
-	const cityVal = $id("select--ville").value;
-	const keywVal = $id("select--categories").value;
-	const cityStr = cityVal ? `[data-city*="${cityVal}"]` : "";
-	const keywStr = keywVal ? `[data-keyword*="${keywVal}"]` : "";
-	const articlesHide = (cityStr || keywStr) ? $q(`article:not(${cityStr}${keywStr})`) : $q(`article`) ;
-	const articlesShow = (cityStr || keywStr) ? $q(`article${cityStr}${keywStr}`) : $q(`article`);
+	const 
+		cityVal = $id("select--ville").value,
+		keywVal = $id("select--categories").value,
+		cityStr = cityVal ? `[data-city*="${cityVal}"]` : "",
+		keywStr = keywVal ? `[data-keyword*="${keywVal}"]` : "",
+		articlesHide = (cityStr || keywStr) ? $q(`article:not(${cityStr}${keywStr})`) : $q(`article`),
+		articlesShow = (cityStr || keywStr) ? $q(`article${cityStr}${keywStr}`) : $q(`article`);
 	$q(".card").forEach(show);
 	articlesHide.forEach(hide);
 	articlesShow.forEach(show);
@@ -123,12 +121,14 @@ function filtrerArticles() {
 	$id("text--resultats").innerHTML = (0 == articlesShow.length ? "aucun" : articlesShow.length) + "&nbsp;r&eacute;sultat" + (1 < articlesShow.length ? "s" : "");
 	masonry && masonry.recalculate(!0, !0);
 }
-function goSearch() {
-	const input = $id('input--search');
-	const selectedOption = $q(`#searchlist option[value="${input.value}"]`);
+/*function goSearch() {
+	const
+		input = $id("input--search"),
+		selectedOption = $q(`#searchlist option[value="${input.value}"]`);
 	if (selectedOption) {
-	  const dataType = selectedOption.dataset.type;
-	  const value = selectedOption.value;
+	  const 
+		dataType = selectedOption.dataset.type,
+		value = selectedOption.value;
 	  switch(dataType) {
 		  case "category": {
 			  $q(`article:not([data-keyword*="${value}"])`).forEach(hide);
@@ -147,9 +147,9 @@ function goSearch() {
 	} else {
 	  console.log("No option selected");
 	}
-	input.value= '';
+	input.value = "";
 	input.blur();
-}
+}*/
 
 function selectAllText(artOrCard) {
 	if (window.getSelection) {
@@ -162,11 +162,11 @@ function selectAllText(artOrCard) {
 	}
 }
 
-function scrollTo(elem) {
+/*function scrollTo(elem) {
 	show(elem);
 	elem.classList.add("highlight");
 	elem.scrollIntoView({behavior: "smooth"});
-}
+}*/
 
 function toMap(event, addressLink) {
 	const isDesktop = (!/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4)));
@@ -180,11 +180,11 @@ function toMap(event, addressLink) {
 }
 
 function toClipboard(text) {
-	const copylistener = (e) => {
-		e.preventDefault();
-		if (e.clipboardData) {
-			e.clipboardData.setData("text/plain", text.innerText);
-			e.clipboardData.setData("text/html", text.innerHTML);
+	const copylistener = event => {
+		event.preventDefault();
+		if (event.clipboardData) {
+			event.clipboardData.setData("text/plain", text.innerText);
+			event.clipboardData.setData("text/html", text.innerHTML);
 			document.documentElement.style.setProperty("--copy-opacity", "1");
 			setTimeout(() => {
 				document.documentElement.style.setProperty("--copy-opacity", "0");
@@ -197,7 +197,7 @@ function toClipboard(text) {
 }
 
 function printArticle(article) {
-	const response = confirm("Par souci d'économie, le document a été passé en niveau de gris.\n\nÉconomisez du papier et privilégiez la copie numérique\nou activez le recto/verso !\n\nJe souhaite imprimer ?");
+	const response = confirm("Par souci d’économie, le document a été passé en niveau de gris.\n\nÉconomisez du papier et privilégiez la copie numérique\nou activez le recto/verso !\n\nJe souhaite imprimer ?");
 	if (!response) return false;
 	const fr = document.createElement("iframe");
 	fr.name = "fr";
@@ -235,26 +235,21 @@ function showQRCode(artOrCard, isCard) {
 }
 
 function hideQRCode() {
-	let img = $q("#qr-code img");
+	const img = $q("#qr-code img");
 	img && img[0].remove();
 	hide($id("qr-code"));
 }
 
 function getVCARDUrlParamAndDownload() {
-	const params = new URLSearchParams(window.location.search),
-		  vcarddata = params.get("vcard"),
-		  index = params.get("i");
-	if (vcarddata) {
-		if (index) {
-			generateAndDownloadVCFfile($q(`[data-vcard=${vcarddata}]`).$q(".card")[+index], true)
-		} else {
-			generateAndDownloadVCFfile($q(`[data-vcard=${vcarddata}]`), false)
-		}
-	}
+	const 
+		params = new URLSearchParams(window.location.search),
+		vcarddata = params.get("vcard"),
+		index = params.get("i");
+	if (vcarddata) generateAndDownloadVCFfile(index ? $q(`[data-vcard=${vcarddata}]`)[0].$q(".card")[+index] : $q(`[data-vcard=${vcarddata}]`)[0], index ? true : false);
 }
 
 function generateAndDownloadVCFfile(artOrCard, isCard) {
-	function convertName(str){
+	function convertName(str) {
 		return str.trim().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^A-Z0-9\s-_]/gi, "").replace(/[\s_-]+/g, "_");
 	}
 	const
@@ -275,7 +270,7 @@ function generateAndDownloadVCFfile(artOrCard, isCard) {
 	newLink.remove();
 }
 
-function generateVCARDtext (artOrCard, isCard) {
+function generateVCARDtext(artOrCard, isCard) {
 	const
 		parentArticle = isCard ? artOrCard.parentElement : artOrCard,
 		vcard_name = parentArticle.getAttribute("data-vcard"),
@@ -304,7 +299,7 @@ function generateVCARDtext (artOrCard, isCard) {
 		content.filter(el => el.classList.contains("url")).forEach(function(n) {
 			vcard_text += "URL:" + n.href + "\n"
 		});
-		content.filter(el => el.className.match(/^facebook|instagram|twitter$/)).forEach(function(n) {
+		content.filter(el => el.className.match(/^facebook|instagram|twitter|youtube|linkedin$/)).forEach(function(n) {
 			const rs = n.classList.value;
 			vcard_text += `X-SOCIALPROFILE;type=${rs};x-user=${rs}user:${n.href}\n`
 		});
@@ -346,14 +341,14 @@ function generateVCARDtext (artOrCard, isCard) {
 		return elementsBeforeFirstCard
 	}
 	if (isCard) {
-		const cardContent = artOrCard.$q("h3, .adr, .url, .tel, .email, .facebook, .twitter, .instagram");
+		const cardContent = artOrCard.$q("h3, .adr, .url, .tel, .email, .facebook, .twitter, .instagram, .linkedin, .youtube");
 		feed = [...extractBeforeFirstCard(), ...cardContent];
 		return generateCard(feed, isCard);
 	} else {
 		if (firstCard) {
 			feed = [];
 			parentArticle.querySelectorAll(".card").forEach(card => {
-				const cardContent = card.$q("h3, .adr, .url, .tel, .email, .facebook, .twitter, .instagram");
+				const cardContent = card.$q("h3, .adr, .url, .tel, .email, .facebook, .twitter, .instagram, .linkedin, .youtube");
 				feed.push([...extractBeforeFirstCard(), ...cardContent]);
 			});
 			let fusionnedVcard = "";
@@ -362,7 +357,7 @@ function generateVCARDtext (artOrCard, isCard) {
 			}
 			return fusionnedVcard
 		} else {
-			feed = parentArticle.$q("h2, h2 p, h3, .adr, .url, .tel, .email, .facebook, .twitter, .instagram");
+			feed = parentArticle.$q("h2, h2 p, h3, .adr, .url, .tel, .email, .facebook, .twitter, .instagram, .linkedin, .youtube");
 			return generateCard(feed);
 		}
 	}
@@ -392,19 +387,19 @@ function setSelectableLetters() {
 
 function populateDatalist() {
 	function createItem(text,val,type) {
-		let item = document.createElement("option");
+		const item = document.createElement("option");
 		item.value = val;
 		item.textContent = text;
 		item.setAttribute("data-type",type)
 		$id("searchlist").appendChild(item);
 	}
 	$id("select--categories").$q("option").forEach((opt, i) => {
-		if (i === 0) return
-		createItem(opt.innerText,opt.value,"category")
+		if (i === 0) return;
+		createItem(opt.innerText,opt.value,"category");
 	});
 	$id("select--ville").$q("option").forEach((opt, i) => {
-		if (i === 0) return
-		createItem(opt.innerText,opt.value,"city")
+		if (i === 0) return;
+		createItem(opt.innerText,opt.value,"city");
 	});
 	$q("article").forEach(article => createItem(article.querySelector("h2").innerText,article.dataset.vcard,"title"));
 }
@@ -415,7 +410,7 @@ function setLocalities() {
 		const 
 			article = city.closest("article"),
 			card = city.closest(".card"),
-			cityText = city.innerText.trim().toUpperCase().split(" CEDEX")[0],
+			cityText = city.innerText.trim().toUpperCase().split(" CEDEX")[0].replace(/[\u0027\u02BC]/g,"’"),
 			cityNb = villes[cityText];
 		if (cityNb) {
 			if (!citiesArray.some(c => c.nb === cityNb)) citiesArray.push({ text: cityText, nb: cityNb });
@@ -430,10 +425,8 @@ function setLocalities() {
 		}
 	});
 	citiesArray.sort((a, b) => {
-		const removePronoun = (text) => text.replace(/^L([AE] |')/, '');
-		const normalizedA = removePronoun(a.text);
-		const normalizedB = removePronoun(b.text);
-		return normalizedA.localeCompare(normalizedB);
+		const removePronoun = t => t.replace(/^L([AE] |[\u2019\u0027\u02BC])/, "");
+		return removePronoun(a.text).localeCompare(removePronoun(b.text));
 	});
 	citiesArray.forEach(city => {
 		const option = document.createElement("option");
@@ -448,19 +441,19 @@ function setLocalities() {
 function setTheme(theme) {
 	if (theme === "dark") {
 		document.documentElement.classList.add("theme-dark");
-		$id('chk--theme-switch').setAttribute("checked","true");
-		$id('chk--theme-switch').setAttribute("aria-label","Passer au thème clair");
+		$id("chk--theme-switch").setAttribute("checked","true");
+		$id("chk--theme-switch").setAttribute("aria-label","Passer au thème clair");
 	} else {
 		document.documentElement.classList.remove("theme-dark");
-		$id('chk--theme-switch').removeAttribute("checked","true");
-		$id('chk--theme-switch').setAttribute("aria-label","Passer au thème sombre");
+		$id("chk--theme-switch").removeAttribute("checked","true");
+		$id("chk--theme-switch").setAttribute("aria-label","Passer au thème sombre");
 	}
 	if (isLocalStorageAvailable) localStorage.setItem("theme", theme);
 }
 
 function themeChecking() {
 	if (window.matchMedia) {
-		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+		window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
 			const pref = event.matches ? "dark" : "light";
 			setTheme(pref);
 		});
@@ -474,11 +467,9 @@ function themeChecking() {
 
 var hoveredCardOrArticle;
 function handleTouchOrHover(event) {
-	const  article = event.target.closest("article"),
-		   card = event.target.closest(".card");
-	if (event.type == "mouseover" && !$id("qr-code").classList.contains("hide")) {
-		return
-	}
+	const article = event.target.closest("article"),
+		  card = event.target.closest(".card");
+	if (event.type == "mouseover" && !$id("qr-code").classList.contains("hide")) return;
 	if (!article) {
 		hoveredCardOrArticle = false;
 		hide($id("modale"));
@@ -494,7 +485,7 @@ function handleTouchOrHover(event) {
 			show($id("modale"));
 		}
 	} else {
-		let hasCard = article.querySelector(".card");
+		const hasCard = article.querySelector(".card");
 		if (hasCard) {
 			hide($id("btn-qrcode"));
 		} else {
@@ -513,7 +504,7 @@ function handleClick(event) {
 	const t = event.target,
 		  a = t.closest("article"),
 		  c = t.closest(".card");
-	"chk--theme-switch" == t.id			&& setTheme($id('chk--theme-switch').checked ? "dark" : "light");
+	"chk--theme-switch" == t.id			&& setTheme($id("chk--theme-switch").checked ? "dark" : "light");
 	"btn--to-top" == t.id	 			&& (event.preventDefault(), $id("nav-bar").scrollIntoView({behavior: "smooth"}));
 	t.closest(".adr") 					&& (toMap(event, t.closest(".adr")));
 	t.closest("#btn-selectall")			&& selectAllText(c || a);
@@ -545,12 +536,12 @@ function handleChange(event) {
 }
 
 function handleBeforePrint(event) {
-	/*event.preventDefault();
-	const response = confirm("Par souci d"économie, le document a été passé en niveau de gris.\n\nÉconomisez du papier et privilégiez la copie numérique\nou activez le recto/verso !\n\nJe souhaite imprimer ?");
+	event.preventDefault();
+	const response = confirm("Par souci d’économie, le document a été passé en niveau de gris.\n\nÉconomisez du papier et privilégiez la copie numérique\nou activez le recto/verso !\n\nJe souhaite imprimer ?");
 	if (response) {
 	  window.removeEventListener("beforeprint", handleBeforePrint);
 	  window.print();
-	}*/
+	}
 }
 
 function handleAfterPrint() {
@@ -558,7 +549,7 @@ function handleAfterPrint() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-	//$id("select--categories").selectedIndex = $id("select--ville").selectedIndex = $id("select--AZ").selectedIndex = 0;
+	$id("select--categories").selectedIndex = $id("select--ville").selectedIndex = $id("select--AZ").selectedIndex = 0;
 	$id("text--resultats").innerHTML = $q("article").length + "&nbsp;r&eacute;sultats";
 	//$id("input--search").value = "";
 	setLocalities();
@@ -583,8 +574,8 @@ window.addEventListener("load", () => {
 });
 
 window.addEventListener("resize", debounce(correctHeight, 500));
-window.addEventListener("beforeprint", handleBeforePrint);
-window.addEventListener("afterprint", handleAfterPrint);
+//window.addEventListener("beforeprint", handleBeforePrint);
+//window.addEventListener("afterprint", handleAfterPrint);
 document.addEventListener("touchstart", handleTouchOrHover);
 document.addEventListener("mouseover", handleTouchOrHover);
 document.addEventListener("click", handleClick);
@@ -592,34 +583,34 @@ document.addEventListener("change", handleChange);
 $id("container").addEventListener("scroll", handleScroll);
 //$id("input--search").addEventListener("focus", function() {this.select()});
 
+/*
 function getAway() {
 	function removeCookies(){
-		var nowDate = new Date();
-		nowDate.setDate(nowDate.getDate() - 7)
-		var res = document.cookie;
-		var multiple = res.split(";");
-		for (var i = 0; i < multiple.length; i++) {
-			var key = multiple[i].split("=");
+		const
+			nowDate = new Date(),
+			res = document.cookie,
+			multiple = res.split(";");
+		nowDate.setDate(nowDate.getDate() - 7);
+		for (let i = 0 ; i < multiple.length ; i++) {
+			const key = multiple[i].split("=");
 			document.cookie = key[0] + " =; expires =" + nowDate;
 		}
 	}
 	removeCookies();
-  document.body.innerHTML = "Cette page est inaccessible."
-  document.title="Erreur : Page inaccessible"
-  window.open("https://meteofrance.com/", "_newtab");
-  //sessionStorage.setItem("quick-exit-fired", 1);
-  window.localStorage.removeItem("theme"); 
-  //window.clipboardData.setData("text", '')
-  navigator.clipboard.writeText(" ");
-  setTimeout(function () {
-	 window.location.replace('https://www.google.com/search?q=météo'); 
-  }, 100);
-  
-  //history.replaceState({}, '', 'https://www.google.com/search?q=météo');
-  
-}
+	document.body.innerHTML = "Cette page est inaccessible."
+	document.title="Erreur : Page inaccessible"
+	window.open("https://meteofrance.com/", "_newtab");
+	//sessionStorage.setItem("quick-exit-fired", 1);
+	window.localStorage.removeItem("theme"); 
+	//window.clipboardData.setData("text", "")
+	navigator.clipboard.writeText(" ");
+	setTimeout(function () {
+		window.location.replace("https://www.google.com/search?q=météo"); 
+	}, 100);
+	//history.replaceState({}, "", "https://www.google.com/search?q=météo");
+}*/
 
-/*document.addEventListener('keydown', (e) => {
+/*document.addEventListener("keydown", (e) => {
 	if (e.keyCode === 27 || e.key === "Escape") getAway();
 	if ((e.keyCode === 13 || e.key === "Enter") && e.target.id == "input--search") {
 		goSearch();
