57c84fbe4dfb4b63bf89e8967b397e77e3aa9a32
MTPGI <115469767+MTPGI@users.noreply.github.com>
2024-03-13 00:06:36 +0000

    Update mtpgi38.js

--- js/mtpgi38.js
+++ js/mtpgi38.js
@@ -2,14 +2,14 @@
 
 // SHORTCUT FUNCTIONS
 
-const 
+const
 	$id = id => document.getElementById(id),
 	$q = Element.prototype.$q = function(q) {
 		const r = (this instanceof Element ? this : document).querySelectorAll(q);
 		return r.length ? Array.from(r) : null;
 	},
 	show = el => el.classList.remove("hide"),
-    hide = el => el.classList.add("hide"),
+	hide = el => el.classList.add("hide"),
 	correctHeight = () => document.documentElement.style.setProperty("--vh", window.innerHeight * 0.01 + "px"),
 	debounce = (fn, time) => {
 		time = time || 100;
@@ -22,55 +22,55 @@ const
 
 // GLOBAL VARIABLES
 
-var	masonry;
+var masonry;
 const villes = {
-	"AMPUIS":"007",
-    "BARRAUX":"027",
-    "BEAUREPAIRE":"034",
-    "LE BOURG-D’OISANS":"052",
-    "BOURGOIN-JALLIEU":"053",
-    "CHASSE-SUR-RHÔNE":"087",
-	"CHAVANOZ":"097",
-    "LA CÔTE-SAINT-ANDRÉ":"130",
-    "CRÉMIEU":"138",
-    "CROLLES":"140",
-    "ÉCHIROLLES":"151",
-    "EYBENS":"158",
-    "FONTAINE":"169",
-    "FONTANIL-CORNILLON":"170",
-    "GIÈRES":"179",
-    "GRENOBLE":"185",
-    "L’ISLE-D’ABEAU":"193",
-    "MENS":"226",
-    "MEYLAN":"229",
-    "MONESTIER DE CLERMONT":"242",
-    "LA MURE":"269",
-    "PONTCHARRA":"314",
-    "LE PONT DE BEAUVOISIN":"315",
-    "PONT-DE-CHÉRUY":"316",
-    "LE PONT-DE-CLAIX":"317",
-    "PONT-EN-ROYANS":"319",
-    "ROUSSILLON":"344",
-    "SAINT-ÉGRÈVE":"382",
-    "SAINT-ISMIER":"397",
-    "SAINT-LAURENT-DU-PONT":"412",
-    "SAINT-MARCELLIN":"416",
-    "SAINT-MARTIN-D’HÈRES":"421",
-    "SEYSSINS":"486",
-    "LA TOUR-DU-PIN":"509",
-    "LA TRONCHE":"516",
-    "TULLINS":"517",
-    "VIENNE":"544",
-    "VIF":"545",
-    "VILLARD-BONNOT":"547",
-    "VILLARD-DE-LANS":"548",
-    "VILLEFONTAINE":"553",
-    "VIZILLE":"562",
-    "VOIRON":"563"
+	"AMPUIS": "007",
+	"BARRAUX": "027",
+	"BEAUREPAIRE": "034",
+	"LE BOURG-D’OISANS": "052",
+	"BOURGOIN-JALLIEU": "053",
+	"CHASSE-SUR-RHÔNE": "087",
+	"CHAVANOZ": "097",
+	"LA CÔTE-SAINT-ANDRÉ": "130",
+	"CRÉMIEU": "138",
+	"CROLLES": "140",
+	"ÉCHIROLLES": "151",
+	"EYBENS": "158",
+	"FONTAINE": "169",
+	"FONTANIL-CORNILLON": "170",
+	"GIÈRES": "179",
+	"GRENOBLE": "185",
+	"L’ISLE-D’ABEAU": "193",
+	"MENS": "226",
+	"MEYLAN": "229",
+	"MONESTIER DE CLERMONT": "242",
+	"LA MURE": "269",
+	"PONTCHARRA": "314",
+	"LE PONT DE BEAUVOISIN": "315",
+	"PONT-DE-CHÉRUY": "316",
+	"LE PONT-DE-CLAIX": "317",
+	"PONT-EN-ROYANS": "319",
+	"ROUSSILLON": "344",
+	"SAINT-ÉGRÈVE": "382",
+	"SAINT-ISMIER": "397",
+	"SAINT-LAURENT-DU-PONT": "412",
+	"SAINT-MARCELLIN": "416",
+	"SAINT-MARTIN-D’HÈRES": "421",
+	"SEYSSINS": "486",
+	"LA TOUR-DU-PIN": "509",
+	"LA TRONCHE": "516",
+	"TULLINS": "517",
+	"VIENNE": "544",
+	"VIF": "545",
+	"VILLARD-BONNOT": "547",
+	"VILLARD-DE-LANS": "548",
+	"VILLEFONTAINE": "553",
+	"VIZILLE": "562",
+	"VOIRON": "563"
 };
 
 function filtrerArticles() {
-	const 
+	const
 		cityVal = $id("select--ville").value,
 		keywVal = $id("select--categories").value,
 		cityStr = cityVal ? `[data-city*="${cityVal}"]` : "",
@@ -102,9 +102,9 @@ function toMap(event, addressLink) {
 	const isDesktop = (!/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4)));
 	if (isDesktop && addressLink.href) {
 		event.preventDefault();
-		const 
+		const
 			geoURI = addressLink.href.split(/[:,]/),
-		    lat = geoURI[1], 
+			lat = geoURI[1],
 			lng = geoURI[2];
 		window.open(`https://www.openstreetmap.org/directions?to=${lat},${lng}#map=18/${lat}/${lng}`, "_blank");
 	}
@@ -131,6 +131,7 @@ function toClipboard(el) {
 	newel.querySelectorAll(".email,.tel,.url,.adr,.bus,.info,.hours,.acc,.facebook,.twitter,.instagram,.youtube,.linkedin").forEach(n => {
 		n.innerHTML = classNames[n.className] + " : " + n.innerHTML;
 	});
+
 	function copylistener(event) {
 		event.preventDefault();
 		if (event.clipboardData) {
@@ -153,7 +154,7 @@ function printArticle(article) {
 		const fr = document.createElement("iframe");
 		fr.name = "fr";
 		document.body.appendChild(fr);
-		const 
+		const
 			frameDoc = fr.contentWindow ? fr.contentWindow : fr.contentDocument.document ? fr.contentDocument.document : fr.contentDocument,
 			title = article.$q("h2").innerText,
 			content = article.outerHTML;
@@ -175,7 +176,7 @@ function showQRCode(artOrCard, isCard) {
 		hideQRCode();
 		return
 	}
-	const 
+	const
 		img = $q("#qr-code img"),
 		data = generateVCARDtext(artOrCard, isCard),
 		qr = qrcode(0, "L");
@@ -185,6 +186,7 @@ function showQRCode(artOrCard, isCard) {
 	$id("qr-code").appendChild(qr.createImgTag(5, 0));
 	show($id("qr-code"));
 }
+
 function hideQRCode() {
 	const img = $q("#qr-code img");
 	img && img[0].remove();
@@ -192,7 +194,7 @@ function hideQRCode() {
 }
 
 function getVCARDUrlParamAndDownload() {
-	const 
+	const
 		params = new URLSearchParams(window.location.search),
 		vcarddata = params.get("vcard"),
 		index = params.get("i");
@@ -207,7 +209,9 @@ function generateAndDownloadVCFfile(artOrCard, isCard) {
 		parentArticle = isCard ? artOrCard.parentElement : artOrCard,
 		vcard_name = isCard ? convertName(artOrCard.querySelector("h3").textContent) : parentArticle.getAttribute("data-vcard"),
 		text = generateVCARDtext(artOrCard, isCard),
-		textToBLOB = new Blob([text], { type: "text/vcard" }),
+		textToBLOB = new Blob([text], {
+			type: "text/vcard"
+		}),
 		newLink = document.createElement("a");
 	newLink.download = vcard_name + ".vcf";
 	if (window.webkitURL != null) {
@@ -229,7 +233,7 @@ function generateVCARDtext(artOrCard, isCard) {
 
 	function generateCard(content) {
 		let vcard_text = "BEGIN:VCARD\nVERSION:3.0\nPRODID:-//MTPGI38//NONSGML v1.0//FR\nTZ:+01:00\n";
-		const 
+		const
 			escapeChar = txt => txt.replace(/[\n\r\t]+/g, " ").replace(/\(.+?\)/, "").replace(/([\\,;:])/g, "\\$1"),
 			name = content.find(el => el.nodeName === "H2") || content.find(el => el.nodeName === "H3"),
 			note = name.querySelector("p");
@@ -237,10 +241,10 @@ function generateVCARDtext(artOrCard, isCard) {
 		vcard_text += "REV:" + $id("revision").innerText.split("/").reverse().join("-") + "T00:00:00Z\n";
 		vcard_text += "FN:" + escapeChar(name.innerText) + "\n";
 		if (note) {
-		vcard_text += "NOTE:" + escapeChar(note.innerText) + "\n"
+			vcard_text += "NOTE:" + escapeChar(note.innerText) + "\n"
 		}
-		content.filter(el => el.classList.contains("tel")).forEach(function(n) {	
-			const 
+		content.filter(el => el.classList.contains("tel")).forEach(function(n) {
+			const
 				// telTypes = ["HOME", "WORK", "PREF", "VOICE", "FAX", "MSG", "CELL", "VIDEO", "TEXTPHONE", "TEXT"];
 				telType = (n.href.startsWith("tel:+336") || n.href.startsWith("tel:06")) ? "CELL" : "VOICE",
 				pref = n.classList.contains("pref") ? ";PREF=1" : "";
@@ -257,19 +261,19 @@ function generateVCARDtext(artOrCard, isCard) {
 			vcard_text += `X-SOCIALPROFILE;type=${rs};x-user=${rs}user:${n.href}\n`
 		});
 		content.filter(el => el.classList.contains("adr")).forEach(n => {
-		  const postOfficeBox = n.querySelector(".post-office-box"),
+			const postOfficeBox = n.querySelector(".post-office-box"),
 				extendedAddress = n.querySelector(".extended-address"),
 				streetAddress = n.querySelector(".street-address"),
 				locality = n.querySelector(".locality"),
 				postalCode = n.querySelector(".postal-code"),
-				getContent = c => c ? c.innerText.replace(/([\\,;:])/g, "\\$1").replace(/[\n\r]+/, "\\n") :	"",
-				adr = "ADR:" 
-					  + getContent(postOfficeBox) + ";"
-					  + getContent(extendedAddress) + ";"
-					  + getContent(streetAddress) + ";"
-					  + getContent(locality) + ";;"
-					  + getContent(postalCode) 
-					  + ";FRANCE\n",
+				getContent = c => c ? c.innerText.replace(/([\\,;:])/g, "\\$1").replace(/[\n\r]+/, "\\n") : "",
+				adr = "ADR:" +
+				getContent(postOfficeBox) + ";" +
+				getContent(extendedAddress) + ";" +
+				getContent(streetAddress) + ";" +
+				getContent(locality) + ";;" +
+				getContent(postalCode) +
+				";FRANCE\n",
 				geoURI = n.href;
 			if (postOfficeBox || extendedAddress || streetAddress || locality || postalCode) {
 				vcard_text += adr
@@ -285,7 +289,7 @@ function generateVCARDtext(artOrCard, isCard) {
 
 	function extractBeforeFirstCard() {
 		const elementsBeforeFirstCard = [],
-			  childNodes = firstCard.parentNode.$q("*");
+			childNodes = firstCard.parentNode.$q("*");
 		for (let node of childNodes) {
 			if (node === firstCard) break;
 			elementsBeforeFirstCard.push(node);
@@ -312,7 +316,7 @@ function generateVCARDtext(artOrCard, isCard) {
 }
 
 // INIT FUNCTIONS, REUSED LATER
-	
+
 function setSelectableLetters() {
 	const arrayOfLetters = [];
 	document.querySelectorAll("article:not(.hide)").forEach(article => {
@@ -329,15 +333,18 @@ function setSelectableLetters() {
 }
 
 function setLocalities() {
-	const citiesArray = []; 
+	const citiesArray = [];
 	document.querySelectorAll(".locality").forEach(city => {
-		const 
+		const
 			article = city.closest("article"),
 			card = city.closest(".card"),
-			cityText = city.innerText.trim().toUpperCase().split(" CEDEX")[0].replace(/[\u0027\u02BC]/g,"’"),
+			cityText = city.innerText.trim().toUpperCase().split(" CEDEX")[0].replace(/[\u0027\u02BC]/g, "’"),
 			cityNb = villes[cityText];
 		if (cityNb) {
-			if (!citiesArray.some(c => c.nb === cityNb)) citiesArray.push({ text: cityText, nb: cityNb });
+			if (!citiesArray.some(c => c.nb === cityNb)) citiesArray.push({
+				text: cityText,
+				nb: cityNb
+			});
 			if (article.dataset.city) {
 				article.dataset.city += `,${cityNb}`;
 			} else {
@@ -345,7 +352,7 @@ function setLocalities() {
 			}
 			if (card) card.dataset.city = cityNb;
 		} else {
-		  console.error(cityText+ " has no match in the array")
+			console.error(cityText + " has no match in the array")
 		}
 	});
 	citiesArray.sort((a, b) => {
@@ -365,12 +372,12 @@ function setLocalities() {
 function setTheme(theme) {
 	if (theme === "dark") {
 		document.documentElement.classList.add("theme-dark");
-		$id("chk--theme-switch").setAttribute("checked","true");
-		$id("chk--theme-switch").setAttribute("aria-label","Passer au thème clair");
+		$id("chk--theme-switch").setAttribute("checked", "true");
+		$id("chk--theme-switch").setAttribute("aria-label", "Passer au thème clair");
 	} else {
 		document.documentElement.classList.remove("theme-dark");
-		$id("chk--theme-switch").removeAttribute("checked","true");
-		$id("chk--theme-switch").setAttribute("aria-label","Passer au thème sombre");
+		$id("chk--theme-switch").removeAttribute("checked", "true");
+		$id("chk--theme-switch").setAttribute("aria-label", "Passer au thème sombre");
 	}
 	if (isLocalStorageAvailable) localStorage.setItem("theme", theme);
 }
@@ -393,7 +400,7 @@ var hoveredCardOrArticle;
 
 function handleTouchOrHover(event) {
 	const article = event.target.closest("article"),
-		  card = event.target.closest(".card");
+		card = event.target.closest(".card");
 	if (event.type == "mouseover" && !$id("qr-code").classList.contains("hide")) return;
 	if (!article) {
 		hoveredCardOrArticle = false;
@@ -427,19 +434,22 @@ function handleTouchOrHover(event) {
 
 function handleClick(event) {
 	const t = event.target,
-		  a = t.closest("article"),
-		  c = t.closest(".card");
-	"chk--theme-switch" == t.id			&& setTheme($id("chk--theme-switch").checked ? "dark" : "light");
-	"btn--to-top" == t.id	 			&& (event.preventDefault(), $id("nav-bar").scrollIntoView({behavior: "smooth"}));
-	t.closest(".adr") 					&& (toMap(event, t.closest(".adr")));
-	t.closest("#btn-selectall")			&& selectAllText(c || a);
-	t.closest("#btn-copy") 				&& toClipboard(c || a);
-	t.closest("#btn-savecontact") 		&& generateAndDownloadVCFfile(c || a, c ? true : false);
-	t.closest("#btn-print") 			&& printArticle(a);
-	t.closest("#btn-qrcode") 			&& showQRCode(c || a, c ? true : false);
-	!t.closest("#apropos") 				&& $id("apropos").removeAttribute("open");
+		a = t.closest("article"),
+		c = t.closest(".card");
+	"chk--theme-switch" == t.id && setTheme($id("chk--theme-switch").checked ? "dark" : "light");
+	"btn--to-top" == t.id && (event.preventDefault(), $id("nav-bar").scrollIntoView({
+		behavior: "smooth"
+	}));
+	t.closest(".adr") && (toMap(event, t.closest(".adr")));
+	t.closest("#btn-selectall") && selectAllText(c || a);
+	t.closest("#btn-copy") && toClipboard(c || a);
+	t.closest("#btn-savecontact") && generateAndDownloadVCFfile(c || a, c ? true : false);
+	t.closest("#btn-print") && printArticle(a);
+	t.closest("#btn-qrcode") && showQRCode(c || a, c ? true : false);
+	!t.closest("#apropos") && $id("apropos").removeAttribute("open");
 	!t.closest("#qr-code, #btn-qrcode") && hideQRCode();
 }
+
 function handleScroll(event) {
 	if ($id("container").scrollTop > window.innerHeight) {
 		show($id("btn--to-top"));
@@ -447,6 +457,7 @@ function handleScroll(event) {
 		hide($id("btn--to-top"));
 	}
 }
+
 function handleChange(event) {
 	const t = event.target;
 	if (t.id == "select--categories" || t.id == "select--ville") {
@@ -455,7 +466,9 @@ function handleChange(event) {
 	}
 	if (t.id == "select--AZ") {
 		const targetArticle = document.querySelector(`article[data-vcard^="${t.value}"]:not(.hide)`);
-		targetArticle && targetArticle.scrollIntoView({behavior: "smooth"});
+		targetArticle && targetArticle.scrollIntoView({
+			behavior: "smooth"
+		});
 		$id("select--AZ").selectedIndex = 0;
 	}
 }
@@ -476,8 +489,15 @@ window.addEventListener("load", () => {
 			container: "main",
 			mobileFirst: true,
 			columns: 3,
-			margin: {y: 50,x: 30},
-			breakAt: {1500: 4, 940: 2, 630: 1 }
+			margin: {
+				y: 50,
+				x: 30
+			},
+			breakAt: {
+				1500: 4,
+				940: 2,
+				630: 1
+			}
 		});
 	}
 	correctHeight();
